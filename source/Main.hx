42efbd4f6c2dfa0cdf6f5ee9cd050e59b9593f3f
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-01-21 09:56:58 +0200

    update source/Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -85,7 +85,7 @@ class Main extends Sprite
 
 		var ourSource:String = "assets/videos/DO NOT DELETE OR GAME WILL CRASH/dontDelete.webm";
 
-		#if (web && !desktop)
+		#if web
 		var str1:String = "HTML CRAP";
 		var vHandler = new VideoHandler();
 		vHandler.init1();
@@ -94,14 +94,6 @@ class Main extends Sprite
 		vHandler.init2();
 		GlobalVideo.setVid(vHandler);
 		vHandler.source(ourSource);
-		#elseif desktop
-		var str1:String = "WEBM SHIT"; 
-		var webmHandle = new WebmHandler();
-		webmHandle.source(ourSource);
-		webmHandle.makePlayer();
-		webmHandle.webm.name = str1;
-		addChild(webmHandle.webm);
-		GlobalVideo.setWebm(webmHandle);
 		#end
 
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
