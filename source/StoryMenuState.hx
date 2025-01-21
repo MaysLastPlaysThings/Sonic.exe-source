fd7fca7e4b6a0fd1cc949da7eef201e59809ba8d
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-01-21 12:19:35 +0200

    update source/StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -210,9 +210,11 @@ class StoryMenuState extends MusicBeatState
                 PlayState.campaignScore = 0;
                 new FlxTimer().start(1, function(tmr:FlxTimer)
                 {
-                    //LoadingState.loadAndSwitchState(new PlayState(), true); //save this code for the cutsceneless build of the game
-                    var video:MP4Handler = new MP4Handler();
-                    video.playMP4(Paths.video('tooslowcutscene1'), new PlayState()); 
+                    var video:VideoHandler = new VideoHandler();
+                    video.playVideo(Paths.video('tooslowcutscene1'));
+                    video.finishCallback = function() {
+                    LoadingState.loadAndSwitchState(new PlayState(), true);
+                    }
                 });
                 
             }
